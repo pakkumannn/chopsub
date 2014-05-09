6c1f5e5a6f8ca750f5959749d8d2fc1b63fe77a4
Pierre-Antoine Cormerais <p.antoine.cormerais@gmail.com>
2014-05-09 10:50:16 +0200

    modification du bouton de connexion + préparation du form commande

--- commande.php
+++ commande.php
@@ -5,7 +5,7 @@ include "connexion.php";
 $bdd = connexion();
 ?>
 <head>
-<link href="../chopsub/css/StyleIdenti.css" rel="stylesheet" media="all" type="text/css">
+<link href="../chopsub/css/StyleComm.css" rel="stylesheet" media="all" type="text/css">
 </head>
 
 <body>
@@ -44,46 +44,65 @@ if ($donnees1['nb1']==1) {
 	</div>
 </div>
 <div id=page>
-	<div id=MenuP>
-		<div id=Option1>
-			<div id=bouton1 onclick="self.location.href='commande.php'">
-				commander
-			</div>
-		</div>
-		<div id=Option2>	
-			<div id=bouton2 onclick="self.location.href='commande.php'">
-				visualiser ma commande
-			</div>
-		</div>
+
+<?php
+$connection->closeCursor();
+$menu1 = $bdd->query("SELECT COUNT(*) as com FROM commande where nom='".$login."'");
+$donnees1 = $menu1->fetch();
+if ($donnees1['com']!=0) {
+        echo "Vous avez deja passe commande";
+        echo "</duv>";
+        echo "<div id=footer>";
+        echo    "<a href='identification.php'> <input type='button' value='Accueil'></a>";
+        echo "</div>";
+}else
+{
+$menu1->closeCursor();
+echo allo;
+?>
+        <div id=formulaire>
+        <form action="verifPerso.php" method="post">
+
+        <a href="identification.php"> <input type="button" value="Accueil"></a>
+        <input type="submit" value="Valider">
+	</form>
 	</div>
-	<?php
-	$connection->closeCursor();
-	$connection2 = $bdd->query("SELECT admin FROM identi where login='".$login."' AND mdp='".$mdp."'");
-	$donnee2 = $connection2->fetch();
-	if ($donnee2['admin']==1) {
-	?>
-	<div id=MenuA>
-		<div id=Option3>
-			<div id=bouton1 onclick="self.location.href='commande.php'">
-				Impression PDF
-			</div>
-		</div>
-		<div id=Option4>
-			<div id=bouton1 onclick="self.location.href='commande.php'">
-				Creer un compte	
-			</div>
-		</div>
-	</div>
-	<?php
-		}
-	?>
+
+<?php
+}
+?>
 </div>
+</body>
+</html>
 
 
-		<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
 }
 else {
-	?>
+?>
+
 		<div id=header>
 			<div id=banniere>
 			<img src="images/banniere.jpg" />
@@ -97,11 +116,6 @@ else {
 				deconnexion	
 			</div>
 		</div>		
-
-	<?php
-}	
-
-$connection2->closeCursor();
-?>
+<?php } ?>
 </body>
 </html>
