4ae259af2887c97271819af1e4b5cb8d40281241
Pierre-Antoine Cormerais <p.antoine.cormerais@gmail.com>
2014-05-14 14:18:02 +0200

    ajout des toutes les étapes de la commande

---
+++ verifCommande.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<?php
+include "connexion.php";
+$bdd = connexion();
+?>
+<head>
+	<link href="../chopsub/css/StyleComm.css" rel="stylesheet" media="all" type="text/css">
+</head>
+
+<body>
+<?php
+echo $_POST['pain'];
+echo $_POST['taille'];
+echo $_POST['viande'];  
+echo $_POST['fromage'];  
+echo $_POST['temperature'];  
+echo $_POST['sauce'];  
+echo $_POST['legume1'];  
+echo $_POST['legume2'];  
+echo $_POST['legume3'];  
+echo $_POST['legume4'];  
+echo $_POST['legume5'];  
+echo $_POST['legume6'];  
+echo $_POST['legume7'];  
+echo $_POST['legume8'];  
+echo $_POST['legume9'];  
+echo $_POST['legume10'];  
+?>
+</body>
