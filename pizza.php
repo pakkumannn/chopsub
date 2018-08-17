b2bf39a9dec4fc53045d725f3b630587e2e7629d
pakkumannn <p.antoine.cormerais@gmail.com>
2018-08-17 09:00:36 +0200

    ajout commande pizza

---
+++ pizza.php
@@ -0,0 +1,438 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<?php
+include "connexion.php";
+$bdd = connexion();
+?>
+<head>
+	<link href="./css/StylePizza.css" rel="stylesheet" media="all" type="text/css">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<!--*********************************************************** -->
+<!--		ajout des variable de session -->
+<!-- ************************************************************ -->
+<?php
+session_start();
+if(isset($_SESSION['login']))
+{
+	$login=$_SESSION['login'];
+	$mdp=$_SESSION['mdp'];
+}
+
+/**********************************************************
+Verification du login mdp
+***********************************************************/
+
+$connection = $bdd->query("SELECT COUNT(*) as nb1 FROM identi where login='".$login."' AND mdp='".$mdp."'");
+$donnees1 = $connection->fetch();
+
+/***********************************************************
+Si login OK
+***********************************************************/
+
+if ($donnees1['nb1']==1) {
+	?>
+<div id=header>
+
+	<div id=typecom>
+        <?php
+                $connection->closeCursor();
+                $connection3 = $bdd->query("SELECT * FROM choix;");
+                $donnee3 = $connection3->fetch();
+                echo "Commande :";
+                echo "</br>";
+                echo "<div id=resultat>";
+                echo $donnee3['choix'];
+                echo "</div>";
+                echo "</br>";
+                echo "Selectionné le ";
+                echo "</br>";
+                echo "<div id=resultat>";
+                echo $donnee3['jour'];
+                echo "</div>";
+        ?>
+	</div>
+
+	<div id=banniere>
+		<img src="images/banniere.jpg" />
+	</div>
+	<div id=connexion>
+		<?php
+		session_start();
+	if(isset($_SESSION['login']))
+	{
+		$login=$_SESSION['login'];
+	}
+	echo 'login : '.$login.'';	
+	?>
+	</div>
+	<div id=deco>
+			<div id=boutonD onclick="self.location.href='deconnexion.php'">
+				déconnexion	
+			</div>
+	</div>
+</div>
+
+
+<?php
+$connection->closeCursor();
+/********************************************************
+si commande deja passe
+********************************************************/
+$menu1 = $bdd->query("select count(*) as com from commande where id ='".$login."'");
+$donnees1 = $menu1->fetch();
+if ($donnees1['com']!=0) {
+	?>	
+	<div id=page>
+			<div id=text>
+				vous avez deja passe une commande
+			</div>
+			</br>
+			<div id=boutonErr onclick="self.location.href='identification.php'">
+				ACCUEIL	
+			</div>
+		</div>		
+<?php
+}else
+{
+$menu1->closecursor();
+/********************************************************
+Si pas de commande
+*********************************************************/
+?>
+<div id=page>
+        <div id=formulaire>
+        <form action="verifSubway.php" method="post">
+	<div id=pizza>		
+		<div class=titre>
+			<b> Basique : </b>
+		</div>		
+		<div class=ligne>
+			<div class=P1>
+				<div class=CocheP1>
+				<input type="radio" name="pain" value="margherita" id="blanc" />
+				</div>
+				<div class=TitreP1>
+				<b>Margherita</b> (sauce tomate, double mozzarella)
+				</div>
+			</div>
+		</div>
+
+		<div class=ligne>
+			<div class=P1>
+				<div class=CocheP1>
+				<input type="radio" name="pain" value="Rein" id="Complet" />
+				</div>
+				<div class=TitreP1>
+				<b>Reine</b> (sauce tomate, mozzarella, champignons, jambon)
+				</div>
+			</div>
+		</div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="hawaienne" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Hawaienne</b> (sauce tomate, mozzarella, jambon, ananas)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="chorizo" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>chorizo</b> (sauce tomate, mozzarella, chorizo)
+                                </div>
+                        </div>
+                </div>
+
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="bolognaise" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Bolognaise</b> (sauce tomate, mozzarella, oigons, boeuf, crème fraiche, herbes de provence)
+                                </div>
+                        </div>
+                </div>
+
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="végétarienne" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Végétarienne</b> (sauce tomate, mozzarella, oigons, poivrons, tomates fraiches, origan)
+                                </div>
+                        </div>
+                </div>
+
+
+                <div class=titre>
+                        <b> Original : </b>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Chicago Groove" id="Complet" />
+                                </div>
+				<div class=TitreP1>
+				<b> Chicago Groove</b> (crème, mozzarella, oigons, poulet, lardon, fourme d'ambert)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Chicago Breakfast" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b> Chicago Breakfast</b> (crème, mozzarella, oigons, poulet, bacon, sauce barbexue)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Addranchie" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Affranchie</b> (sauce BBQ, mozzarella, oigon, merguez, poulet, cheddar )
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="infernale" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Infernale</b> (sauce tomate, mozzarella, poulet, chorizo, merguez, huile pimentée)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="5 fromages" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>5 fromages</b> (sauce tomate, mozzarella, chèvre, fourne d'ambert, reblochon, cheddar)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Chicago Burger" id="Complet" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Chicago Burger</b> (sauce tomate, mozzarella, oigons, boeuf, cheddar, sauce burger)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Dallas BBQ" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Dallas BBQ</b> (sauce BBQ, mozzarella, oignons, boeuf, lardon, bacon)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Al Capone" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Al Capone</b> (sauce tomate, mozzarella, jambon, chorizo, merguez)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Forestière" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Forestiere</b> (crème mozzarella, champignons, oignons, jambon, lardons, origan)
+                                </div>
+                        </div>
+                </div>
+
+                <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Savoyarde" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Savoyarde</b> (crème, mozzarella, oignons, lardons, pommes de terre, reblochon)
+                                </div>
+                        </div>
+                </div>
+
+
+               <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Chèvre-miel" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Chèvre-miel</b> (crème, mozzarella, chèvre, miel, herbes de provence)
+                                </div>
+                        </div>
+                </div>
+
+
+               <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Kebab" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Kebab</b> (souce tomate, mozzarella, oignons, poivrons, emincé de kebab, sauce blanche)
+                                </div>
+                        </div>
+                </div>
+               <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Fajitas" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Fajitas</b> (Sauce tomate, mozza, oignons, poivrons, poulet, crème fraiche, épices mexicaines)
+                                </div>
+                        </div>
+                </div>
+
+               <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Indienne" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Indienne</b> (crème, mozzarella, oignons, poulet, curry, ananas)
+                                </div>
+                        </div>
+                </div>
+
+               <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Michigan" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Michigan</b> (crème, mozzarella, oignons, saumon, aneth)
+                                </div>
+                        </div>
+                </div>
+
+               <div class=ligne>
+                        <div class=P1>
+                                <div class=CocheP1>
+                                <input type="radio" name="pain" value="Marina Bay" />
+                                </div>
+                                <div class=TitreP1>
+                                <b>Marina Bay</b> (Crème, mozzarella, tomates fraiches, thon, oignons, herbe de provence)
+                                </div>
+                        </div>
+                </div>
+	</div>
+
+	<div id=patte>
+        	<div id=titrePatte>
+                	<b>Patte :  </b>
+	         </div>
+        	 <div id=PatteL1>
+                 	<select name="patte">
+                        	<option value="épaisse"> epaisse </option>
+                                <option value="fine"> fine </option>
+                        </select>
+             	 </div>
+        </div>
+
+
+        <div id=patte>
+                <div id=titrePatte>
+                        <b>Taille :  </b>
+                 </div>
+                 <div id=PatteL1>
+                        <select name="patte">
+                                <option value="30 cm"> 30 cm </option>
+                                <option value="40 cm"> 40 cm </option>
+                        </select>
+                 </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+	<div id=footer>
+
+			<div id=bouton1 onclick="self.location.href='identification.php'">
+				ANNULE
+			</div>
+		        <input type="submit" value="VALIDER" id="boutonV">
+	</div>
+	</form>
+	</div>
+
+<?php
+}
+?>
+</div>
+
+<?php
+}
+else {
+?>
+<!--***************************************************
+Si erreur de login
+****************************************************--!>
+		<div id=header>
+			<div id=banniere>
+			<img src="images/banniere.jpg" />
+			</div>
+ 		</div>
+		<div id=page>
+			<div id=text>
+			Erreur sur le mdp ou le login
+			</div>
+			</br>
+			<div id=boutonErr onclick="self.location.href='deconnexion.php'">
+				CONNEXION	
+			</div>
+		</div>		
+<?php } ?>
+</body>
+</html>
---
+++ verifPizza.php
@@ -0,0 +1,412 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<?php
+include "connexion.php";
+$bdd = connexion();
+?>
+<head>
+	<link href="./css/StyleVerifSub.css" rel="stylesheet" media="all" type="text/css">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<!--***********************************************************
+		ajout des variable de session
+************************************************************--!>
+<?php
+session_start();
+if(isset($_SESSION['login']))
+{
+	$login=$_SESSION['login'];
+	$mdp=$_SESSION['mdp'];
+}
+
+/**********************************************************
+Verification du login mdp
+***********************************************************/
+
+$connection = $bdd->query("SELECT COUNT(*) as nb1 FROM identi where login='".$login."' AND mdp='".$mdp."'");
+$donnees1 = $connection->fetch();
+
+/***********************************************************
+Si login OK
+***********************************************************/
+
+if ($donnees1['nb1']==1) {
+	?>
+<div id=header>
+	<div id=banniere>
+		<img src="images/banniere.jpg" />
+	</div>
+	<div id=connexion>
+		<?php
+		session_start();
+	if(isset($_SESSION['login']))
+	{
+		$login=$_SESSION['login'];
+	}
+	echo 'login : '.$login.'';	
+	?>
+	</div>
+	<div id=deco>
+			<div id=boutonD onclick="self.location.href='deconnexion.php'">
+				déconnexion	
+			</div>
+	</div>
+</div>
+
+
+<?php
+$connection->closeCursor();
+/********************************************************
+si commande deja passe
+********************************************************/
+$menu1 = $bdd->query("select count(*) as com from subway where nom='".$login."'");
+$donnees1 = $menu1->fetch();
+if ($donnees1['com']!=0) {
+	?>	
+	<div id=page>
+			<div id=text>
+				vous avez déjà passé une commande
+			</div>
+			</br>
+			<div id=boutonErr onclick="self.location.href='identification.php'">
+				ACCUEIL	
+			</div>
+		</div>		
+<?php
+}else
+{
+$menu1->closecursor();
+/********************************************************
+Si pas de commande Affichage de la page
+*********************************************************/
+?>
+<div id=page>
+<?php
+if ($_POST['pain']=='' || $_POST['taille']=='' || $_POST['viande']=='' || $_POST['fromage']=='' || $_POST['temperature']=='' || $_POST['sauce']=='') {
+    echo "<div class=text>";
+	echo "Vous avez une erreur dans votre commande, vous avez oublié un ingrédient";
+	echo "<div id=footer>";
+	?>
+		<a href="javascript:history.back()">RETOUR</a>
+	<?php
+	echo "</div>";
+	echo "</div>";
+} 
+else {
+?>
+	<div id=text>
+	Voici le résumé de votre commande
+	</div>
+	<div class=ligne1>
+		<div class=col1> PAIN :</div>
+		<div class=col2> <?php echo $_POST['pain']; ?></div>
+	</div>
+	<div class=ligne1>
+		<div class=col1> TAILLE :</div>
+		<div class=col2> <?php echo $_POST['taille']; ?></div>
+	</div>
+	<div class=ligne1>
+		<div class=col1> VIANDE :</div>
+		<div class=col2> <?php echo $_POST['viande']; ?></div>
+	</div>
+	<div class=ligne1>
+		<div class=col1> FROMAGE :</div>
+		<div class=col2> <?php echo $_POST['fromage']; ?></div>
+	</div>
+	<div class=ligne1>
+		<div class=col1> TEMPERATURE :</div>
+		<div class=col2> <?php echo $_POST['temperature']; ?></div>
+	</div>
+	<div class=ligne1>
+		<div class=col1> SAUCE :</div>
+		<div class=col2> <?php echo $_POST['sauce']; ?></div>
+	</div>
+	<div class=ligne1>
+		<div class=col1> LEGUMES :</div>
+		<div class=col2> <?php echo $_POST['legume1']; ?> </div>
+	</div>
+	<?php if ($_POST['legume2']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume2'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume3']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume3'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume4']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume4'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume5']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume5'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume6']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume6'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume7']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume7'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume8']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume8'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume9']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume9'];
+			echo"</div>";
+		echo "</div>";
+	}
+	 if ($_POST['legume10']!=''){
+		echo "<div class=ligne1>";
+			echo "<div class=col2b>";
+				echo $_POST['legume10'];
+			echo"</div>";
+		echo "</div>";
+	}
+
+?>
+
+	<div class=ligne1>
+
+<?php
+
+if ($_POST['taille']=='30 cm') {
+	if ($_POST['viande']=='Teriaky') {
+	$prix='7';
+	}
+	else {
+		if ($_POST['viande']=='Tikka') {
+		$prix='7';
+		}
+		else {
+			if ($_POST['viande']=='Roastbeef') {
+			$prix='7';
+			}
+			else {
+				if ($_POST['viande']=='Dinde') {
+				$prix='6';
+				}
+				else {
+					if ($_POST['viande']=='Jambon') {
+					$prix='6';
+					}
+					else {
+						if ($_POST['viande']=='MELT') {
+						$prix='6,5';
+						}	
+						else {
+							if ($_POST['viande']=='CLUB') {
+							$prix='6,5';
+							}
+							else {
+								if ($_POST['viande']=='B.M.T') {
+								$prix='6,5';
+								}
+								else {
+									if ($_POST['viande']=='Dinde Jambon') {
+									$prix='6';
+									}
+									else {
+										if ($_POST['viande']=='Thon') {
+										$prix='7';
+										}
+										else {
+											if ($_POST['viande']=='Steack vegetarien') {
+											$prix='6,5';
+											}
+											else {
+												if ($_POST['viande']=='Meatball') {
+												$prix='7';
+												}
+												else {
+													 if ($_POST['viande']=='Steak & cheese') {
+													 $prix='7';
+													}
+													else {
+														if ($_POST['viande']=='Spicy Italien') {
+														$prix='6';
+														}
+													}	
+												}
+											}	
+										}
+									}		
+								}
+							}		
+						}
+					}		
+				}	
+			}	
+		}	
+	}
+}
+
+
+if ($_POST['taille']=='15 cm') {
+	if ($_POST['viande']=='Teriaky') {
+	$prix='4,5';
+	}
+	else {
+		if ($_POST['viande']=='Tikka') {
+		$prix='4,5';
+		}
+		else {
+			if ($_POST['viande']=='Roastbeef') {
+			$prix='4,5';
+			}
+			else {
+				if ($_POST['viande']=='Dinde') {
+				$prix='3,5';
+				}
+				else {
+					if ($_POST['viande']=='Jambon') {
+					$prix='3,5';
+					}
+					else {
+						if ($_POST['viande']=='MELT') {
+						$prix='4';
+						}	
+						else {
+							if ($_POST['viande']=='CLUB') {
+							$prix='4';
+							}
+							else {
+								if ($_POST['viande']=='B.M.T') {
+								$prix='4';
+								}
+								else {
+									if ($_POST['viande']=='Dinde Jambon') {
+									$prix='3,5';
+									}
+									else {
+										if ($_POST['viande']=='Thon') {
+										$prix='4,5';
+										}
+										else {
+											if ($_POST['viande']=='Steack vegetarien') {
+											$prix='4';
+											}
+											else {
+												if ($_POST['viande']=='Meatball') {
+												$prix='4,5';
+												}
+												else {
+													 if ($_POST['viande']=='Steak & cheese') {
+													 $prix='4,5';
+													}
+													else {
+														if ($_POST['viande']=='Spicy Italien') {
+														$prix='3,5';
+														}
+													}	
+												}
+											}	
+										}
+									}		
+								}
+							}		
+						}
+					}		
+				}	
+			}	
+		}	
+	}
+}
+
+?>
+
+
+
+
+
+
+
+		<div class=col1> PRIX : </div>
+		<div class=col2> <? echo $prix; ?></div>
+	</div>
+
+
+<?php
+echo "<div id=footer>";
+$pain=$_POST['pain'];
+$taille=$_POST['taille'];
+$viande=$_POST['viande'];
+$fromage=$_POST['fromage'];
+$temperature=$_POST['temperature'];
+$legume1=$_POST['legume1'];
+$legume2=$_POST['legume2'];
+$legume3=$_POST['legume3'];
+$legume4=$_POST['legume4'];
+$legume5=$_POST['legume5'];
+$legume6=$_POST['legume6'];
+$legume7=$_POST['legume7'];
+$legume8=$_POST['legume8'];
+$legume9=$_POST['legume9'];
+$legume10=$_POST['legume10'];
+$sauce=$_POST['sauce'];
+?>
+<a href="javascript:history.back()">RETOUR</a>
+<?php
+echo "<a href='enregSubway.php?pain=".$pain."&taille=".$taille."&viande=".$viande."&fromage=".$fromage."&temperature=".$temperature."&legume1=".$legume1."&legume2=".$legume2."&legume3=".$legume3."&legume4=".$legume4."&legume5=".$legume5."&legume6=".$legume6."&legume7=".$legume7."&legume8=".$legume8."&legume9=".$legume9."&legume10=".$legume10."&sauce=".$sauce."&prix=".$prix."'> ENREGISTER </a>";
+
+
+/*
+echo "<a href='enregCommande.php?pain=".$pain."&taille=".$taille."&viande=".$viande."&fromage=".$fromage."&temperature=".$temperature."&legume1=".$legume1."&legume2=".$legume2."&legume3=".$legume3."&legume4=".$legume4."&legume5=".$legume5."&legume6=".$legume6."&legume7=".$legume7."&legume8=".$legume8."&legume9=".$legume9."&legume10=".$legume10."&sauce=".$sauce."&prix=".$prix."'> <input type='button' value='Enregistrer' id=bouton1></a>";
+*/
+?>
+
+</div>
+</div>
+</div>
+<?php
+}
+}
+
+}
+else {
+?>
+<!--***************************************************
+Si erreur de login
+****************************************************--!>
+		<div id=header>
+			<div id=banniere>
+			<img src="images/banniere.jpg" />
+			</div>
+ 		</div>
+		<div id=page>
+			<div id=text>
+			Erreur sur le mdp ou le login
+			</div>
+			<div id=boutonD onclick="self.location.href='deconnexion.php'">
+				deconnexion	
+			</div>
+		</div>		
+<?php } ?>
+</body>
