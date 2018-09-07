076e1676a44db8c5ae4b27b9a7b186900dcc6145
pakkumannn <p.antoine.cormerais@gmail.com>
2018-09-07 10:23:15 +0200

    correction aff pizza

--- pizza.php
+++ pizza.php
@@ -6,6 +6,7 @@ $bdd = connexion();
 ?>
 <head>
 	<link href="./css/StylePizza.css" rel="stylesheet" media="all" type="text/css">
+	<link href="./css/baniere.css" rel="stylesheet" media="all" type="text/css">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
 
@@ -69,7 +70,7 @@ if ($donnees1['nb1']==1) {
 	?>
 	</div>
 	<div id=deco>
-			<div id=boutonD onclick="self.location.href='deconnexion.php'">
+			<div id=boutonDH onclick="self.location.href='deconnexion.php'">
 				déconnexion	
 			</div>
 	</div>
@@ -366,7 +367,7 @@ Si pas de commande
 	         </div>
         	 <div id=PatteL1>
                  	<select name="patte">
-                        	<option value="épaisse"> epaisse </option>
+                        	<option value="epaisse"> epaisse </option>
                                 <option value="fine"> fine </option>
                         </select>
              	 </div>
